55ee786883ab7beef28836dc9296f6888402888f
ChiamakaUI <55190142+ChiamakaUI@users.noreply.github.com>
2021-10-23 22:57:03 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let submit = document.getElementsByClassName("icon")[0];
+
+submit.addEventListener("click", submitForm);
+let email = document.getElementById("mail");
+
+
+function submitForm(e) {
+  e.preventDefault();
+
+    let emailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    let error = document.getElementsByClassName("error")[0];
+     let errorMessage = document.getElementById("error_message");
+
+  if (email.value == "") {
+    alert("Please, enter an email address to proceed");
+    return;
+  } else {
+    if (email.value.match(emailformat)) {
+      alert("Thank you, we will send a mail as soon as we launch");
+    } else {
+        error.style.display = 'flex'
+        errorMessage.style.display = "block";
+
+        return
+    }
+  }
+
+  email.value = "";
+}
